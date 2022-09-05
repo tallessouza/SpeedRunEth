ecd64b861cb68fd91389607f21a9a1ee2e2399c4
tallessouza <talles-swd@live.com>
2022-09-04 21:59:50 -0300

    Interacting with contract

---
+++ day2/contract_read.js
@@ -0,0 +1,15 @@
+import { getProvider } from "./utils.js"
+import abi from "./abi/abi.json" assert { type: 'json' }
+import { ethers } from "ethers";
+
+const contractAdd = `0x6E2756D5A4780c4d26De0A91f0c0AF5CE77cBC34`
+const provider = getProvider()
+// new ethers.Contract(addres)
+// console.log(provider)
+const contract = new ethers.Contract(
+  contractAdd,
+  abi,
+  provider
+)
+console.log(contract)
+40min
